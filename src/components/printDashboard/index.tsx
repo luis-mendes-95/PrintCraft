f0200018eeda8ef03d8b063e13016eff7dbe5a4b
Luis Mendes <luis.mendes.95@hotmail.com>
2023-07-12 20:30:30 -0300

    feat: color pick to every mold

--- src/components/printDashboard/index.tsx
+++ src/components/printDashboard/index.tsx
@@ -1,24 +1,70 @@
 /* eslint-disable @next/next/no-img-element */
-import { usePrint } from "@/contexts/printContext"
-import { PrintDashboardBase } from "./style"
-import { useState } from "react";
+import { usePrint } from "@/contexts/printContext";
+import { PrintDashboardBase } from "./style";
+import { useState, useEffect } from "react";
 
 const PrintDashboard = () => {
-  const { backgroundFront, backgroundBack, leftSleeve, rightSleeve, setBackgroundFrontImg, setBackgroundBackImg, setBackgroundLeftSleeveImg, setBackgroundRightSleeveImg, setBackgroundCollarImg } = usePrint();
+  const {
+    backgroundFront,
+    backgroundBack,
+    leftSleeve,
+    rightSleeve,
+    setBackgroundFrontImg,
+    setBackgroundBackImg,
+    setBackgroundLeftSleeveImg,
+    setBackgroundRightSleeveImg,
+    setBackgroundCollarImg
+  } = usePrint();
   const [selectedFrontFile, setSelectedFrontFile] = useState(null);
   const [selectedBackFile, setSelectedBackFile] = useState(null);
   const [selectedRightSleeveFile, setSelectedRightSleeveFile] = useState(null);
   const [selectedLeftSleeveFile, setSelectedLeftSleeveFile] = useState(null);
   const [selectedCollarFile, setSelectedCollarFile] = useState(null);
+  const [selectedFrontColor, setSelectedFrontColor] = useState("");
+  const [selectedBackColor, setSelectedBackColor] = useState("");
+  const [selectedRightSleeveColor, setSelectedRightSleeveColor] = useState("");
+  const [selectedLeftSleeveColor, setSelectedLeftSleeveColor] = useState("");
+  const [selectedCollarColor, setSelectedCollarColor] = useState("");
 
-  const handleFrontFileChange = (event: any) => {
+  useEffect(() => {
+    if (selectedFrontColor && backgroundFront) {
+      setBackgroundFrontImg("");
+    }
+  }, [selectedFrontColor, backgroundFront]);
+
+  useEffect(() => {
+    if (selectedBackColor && backgroundBack) {
+      setBackgroundBackImg("");
+    }
+  }, [selectedBackColor, backgroundBack]);
+
+  useEffect(() => {
+    if (selectedRightSleeveColor && rightSleeve) {
+      setBackgroundRightSleeveImg("");
+    }
+  }, [selectedRightSleeveColor, rightSleeve]);
+
+  useEffect(() => {
+    if (selectedLeftSleeveColor && leftSleeve) {
+      setBackgroundLeftSleeveImg("");
+    }
+  }, [selectedLeftSleeveColor, leftSleeve]);
+
+  useEffect(() => {
+    if (selectedCollarColor && leftSleeve) {
+      setBackgroundCollarImg("");
+    }
+  }, [selectedCollarColor, leftSleeve]);
+
+  const handleFrontFileChange = (event) => {
     const file = event.target.files[0];
     setSelectedFrontFile(file);
+    setSelectedFrontColor("");
 
     const reader = new FileReader();
     reader.onload = () => {
       const imageDataUrl = reader.result;
-      console.log('Imagem carregada:', imageDataUrl);
+      console.log("Imagem carregada:", imageDataUrl);
       // Outras ações que você deseja realizar com a imagem carregada
 
       // Atualize o estado ou faça o que for necessário com a imagem carregada
@@ -30,14 +76,15 @@ const PrintDashboard = () => {
     }
   };
 
-  const handleBackFileChange = (event: any) => {
+  const handleBackFileChange = (event) => {
     const file = event.target.files[0];
     setSelectedBackFile(file);
+    setSelectedBackColor("");
 
     const reader = new FileReader();
     reader.onload = () => {
       const imageDataUrl = reader.result;
-      console.log('Imagem carregada:', imageDataUrl);
+      console.log("Imagem carregada:", imageDataUrl);
       // Outras ações que você deseja realizar com a imagem carregada
 
       // Atualize o estado ou faça o que for necessário com a imagem carregada
@@ -49,14 +96,15 @@ const PrintDashboard = () => {
     }
   };
 
-  const handleRightSleeveFileChange = (event: any) => {
+  const handleRightSleeveFileChange = (event) => {
     const file = event.target.files[0];
     setSelectedRightSleeveFile(file);
+    setSelectedRightSleeveColor("");
 
     const reader = new FileReader();
     reader.onload = () => {
       const imageDataUrl = reader.result;
-      console.log('Imagem carregada:', imageDataUrl);
+      console.log("Imagem carregada:", imageDataUrl);
       // Outras ações que você deseja realizar com a imagem carregada
 
       // Atualize o estado ou faça o que for necessário com a imagem carregada
@@ -68,14 +116,15 @@ const PrintDashboard = () => {
     }
   };
 
-  const handleLeftSleeveFileChange = (event: any) => {
+  const handleLeftSleeveFileChange = (event) => {
     const file = event.target.files[0];
     setSelectedLeftSleeveFile(file);
+    setSelectedLeftSleeveColor("");
 
     const reader = new FileReader();
     reader.onload = () => {
       const imageDataUrl = reader.result;
-      console.log('Imagem carregada:', imageDataUrl);
+      console.log("Imagem carregada:", imageDataUrl);
       // Outras ações que você deseja realizar com a imagem carregada
 
       // Atualize o estado ou faça o que for necessário com a imagem carregada
@@ -87,14 +136,15 @@ const PrintDashboard = () => {
     }
   };
 
-  const handleCollarFileChange = (event: any) => {
+  const handleCollarFileChange = (event) => {
     const file = event.target.files[0];
     setSelectedCollarFile(file);
+    setSelectedCollarColor("");
 
     const reader = new FileReader();
     reader.onload = () => {
       const imageDataUrl = reader.result;
-      console.log('Imagem carregada:', imageDataUrl);
+      console.log("Imagem carregada:", imageDataUrl);
       // Outras ações que você deseja realizar com a imagem carregada
 
       // Atualize o estado ou faça o que for necessário com a imagem carregada
@@ -113,10 +163,20 @@ const PrintDashboard = () => {
         <button>Cor Sólida</button>
         <span>OU</span>
         <input type="file" onChange={handleFrontFileChange} placeholder="Selecionar Imagem do Background" />
+        <input type="color" value={selectedFrontColor} onChange={(event) => setSelectedFrontColor(event.target.value)} />
         <span className="sizeRecommended">Tamanho Recomendado: 61cm x 79cm</span>
         <div className="divProductTemplate">
-          <img className="imgBackground" src={selectedFrontFile ? URL.createObjectURL(selectedFrontFile) : ''} alt="backgroundImage" />
-          <img className="imgMold" src="https://res.cloudinary.com/dwadq5lzp/image/upload/v1689179699/camBasEncaixeFrontal_sw1u91.png" alt="moldImage" />
+          <img
+            className="imgBackground"
+            src={selectedFrontFile ? URL.createObjectURL(selectedFrontFile) : ""}
+            style={{ backgroundColor: selectedFrontColor }}
+            alt="backgroundImage"
+          />
+          <img
+            className="imgMold"
+            src="https://res.cloudinary.com/dwadq5lzp/image/upload/v1689179699/camBasEncaixeFrontal_sw1u91.png"
+            alt="moldImage"
+          />
         </div>
       </div>
 
@@ -125,9 +185,19 @@ const PrintDashboard = () => {
         <button>Cor Sólida</button>
         <span>OU</span>
         <input type="file" onChange={handleBackFileChange} placeholder="Selecionar Imagem do Background" />
+        <input type="color" value={selectedBackColor} onChange={(event) => setSelectedBackColor(event.target.value)} />
         <div className="divProductTemplate">
-          <img className="imgBackground" src={selectedBackFile ? URL.createObjectURL(selectedBackFile) : ''} alt="backgroundImage" />
-          <img className="imgMold" src="https://res.cloudinary.com/dwadq5lzp/image/upload/v1689180319/camBasEncaixeVerso_xhmp37.png" alt="moldImage" />
+          <img
+            className="imgBackground"
+            src={selectedBackFile ? URL.createObjectURL(selectedBackFile) : ""}
+            style={{ backgroundColor: selectedBackColor }}
+            alt="backgroundImage"
+          />
+          <img
+            className="imgMold"
+            src="https://res.cloudinary.com/dwadq5lzp/image/upload/v1689180319/camBasEncaixeVerso_xhmp37.png"
+            alt="moldImage"
+          />
         </div>
       </div>
 
@@ -136,9 +206,23 @@ const PrintDashboard = () => {
         <button>Cor Sólida</button>
         <span>OU</span>
         <input type="file" onChange={handleRightSleeveFileChange} placeholder="Selecionar Imagem do Background" />
+        <input
+          type="color"
+          value={selectedRightSleeveColor}
+          onChange={(event) => setSelectedRightSleeveColor(event.target.value)}
+        />
         <div className="divProductTemplate">
-          <img className="imgBackgroundSleeve" src={selectedRightSleeveFile ? URL.createObjectURL(selectedRightSleeveFile) : ''} alt="backgroundImage" />
-          <img className="imgMoldSleeve" src="https://res.cloudinary.com/dwadq5lzp/image/upload/v1689180735/camBasEncaixeManga_uhhx27.png" alt="moldImage" />
+          <img
+            className="imgBackgroundSleeve"
+            src={selectedRightSleeveFile ? URL.createObjectURL(selectedRightSleeveFile) : ""}
+            style={{ backgroundColor: selectedRightSleeveColor }}
+            alt="backgroundImage"
+          />
+          <img
+            className="imgMoldSleeve"
+            src="https://res.cloudinary.com/dwadq5lzp/image/upload/v1689180735/camBasEncaixeManga_uhhx27.png"
+            alt="moldImage"
+          />
         </div>
       </div>
 
@@ -147,9 +231,23 @@ const PrintDashboard = () => {
         <button>Cor Sólida</button>
         <span>OU</span>
         <input type="file" onChange={handleLeftSleeveFileChange} placeholder="Selecionar Imagem do Background" />
+        <input
+          type="color"
+          value={selectedLeftSleeveColor}
+          onChange={(event) => setSelectedLeftSleeveColor(event.target.value)}
+        />
         <div className="divProductTemplate">
-          <img className="imgBackgroundSleeve" src={selectedLeftSleeveFile ? URL.createObjectURL(selectedLeftSleeveFile) : ''} alt="backgroundImage" />
-          <img className="imgMoldSleeve" src="https://res.cloudinary.com/dwadq5lzp/image/upload/v1689180735/camBasEncaixeManga_uhhx27.png" alt="moldImage" />
+          <img
+            className="imgBackgroundSleeve"
+            src={selectedLeftSleeveFile ? URL.createObjectURL(selectedLeftSleeveFile) : ""}
+            style={{ backgroundColor: selectedLeftSleeveColor }}
+            alt="backgroundImage"
+          />
+          <img
+            className="imgMoldSleeve"
+            src="https://res.cloudinary.com/dwadq5lzp/image/upload/v1689180735/camBasEncaixeManga_uhhx27.png"
+            alt="moldImage"
+          />
         </div>
       </div>
 
@@ -158,12 +256,25 @@ const PrintDashboard = () => {
         <button>Cor Sólida</button>
         <span>OU</span>
         <input type="file" onChange={handleCollarFileChange} placeholder="Selecionar Imagem do Background" />
+        <input
+          type="color"
+          value={selectedCollarColor}
+          onChange={(event) => setSelectedCollarColor(event.target.value)}
+        />
         <div className="divProductTemplate">
-          <img className="imgBackgroundSleeve" src={selectedCollarFile ? URL.createObjectURL(selectedCollarFile) : ''} alt="backgroundImage" />
-          <img className="imgMoldSleeve" src="https://res.cloudinary.com/dwadq5lzp/image/upload/v1689182227/camBasEncaixeGola_plfkhq.png" alt="moldImage" />
+          <img
+            className="imgBackgroundSleeve"
+            src={selectedCollarFile ? URL.createObjectURL(selectedCollarFile) : ""}
+            style={{ backgroundColor: selectedCollarColor }}
+            alt="backgroundImage"
+          />
+          <img
+            className="imgMoldSleeve"
+            src="https://res.cloudinary.com/dwadq5lzp/image/upload/v1689182227/camBasEncaixeGola_plfkhq.png"
+            alt="moldImage"
+          />
         </div>
       </div>
-
     </PrintDashboardBase>
   );
 };
